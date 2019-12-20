85cf189bc2e00765978bdc22912a526a1a7c1b4e
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-20 12:30:07 +0100

    Create Blocks.java

---
+++ Blocks.java
@@ -0,0 +1,32 @@
+public abstract class Blocks {
+    protected String description;
+    protected String name;
+    protected boolean isDestructable; 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public boolean getDestructability() {
+        return isDestructable;
+    }
+
+
+
+    public void playCard(){
+
+
+
+
+    }
+
+
+
+
+
+
+}
