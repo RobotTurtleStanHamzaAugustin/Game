7876a68041b22f85591c1454a83a8db80c0eb164
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-20 12:01:45 +0100

    Add files via upload

---
+++ IceBlock.java
@@ -0,0 +1,7 @@
+class IceBlock extends Blocks {
+    public IceBlock() {
+        this.description = "Pose un bloc de glace sur la grille";
+        this.name =  "Ice Block";
+        this.isDestructable =  true;
+    }
+}
